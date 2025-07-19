7635e41b63a8264427d578e1f11b58c9e1fc2815
Reaishma N <hayagreevar9018@gmail.com>
2025-07-19 20:23:59 +0530

    Create Application.rb

---
+++ rails_nlp_api/config/Application.rb
@@ -0,0 +1,23 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module RailsNlpApi
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
+    # Configuration for the application, engines, and railties goes here.
+    config.api_only = true
+    
+    # CORS configuration
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+  end
+end
